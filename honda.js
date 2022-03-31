a13890f141aa4b58b521a5f9507f9410cab30e56
Thien Duc <ducthiennguyen1601@gmail.com>
2022-03-31 22:02:14 +0700

    first commit

---
+++ honda.js
@@ -0,0 +1,154 @@
+const blue = document.querySelector('.item .blue')
+const image = document.querySelector('.item.item-2 img')
+blue.onmouseover = function(){
+  
+   image.style.transform ='scale(1.2)'
+   image.style.transition ='all 0.5s linear'
+
+}
+blue.onmouseout = function(){
+  
+     image.style.transform ='scale(1)'
+     image.style.transition ='all 0.5s linear'
+}
+image.onmouseover = function(){
+     image.style.transform ='scale(1.2)'
+     image.style.transition ='all 0.5s linear'
+
+
+}
+image.onmouseout = function(){
+    image.style.transform ='scale(1)'
+    image.style.transition ='all 0.5s linear'
+
+}
+
+const red = document.querySelectorAll('.item .red')
+const black = document.querySelector('.item .black')
+const white = document.querySelector('.item .white')
+const image4 = document.querySelector('.item.item-4 img')
+const image6 = document.querySelector('.item.item-6 img')
+const image10 = document.querySelector('.item.item-10 img')
+const image11 = document.querySelector('.item.item-11 img')
+const image14 = document.querySelector('.item.item-14 img')
+
+red[0].onmouseover = function(){
+  
+    image4.style.transform ='scale(1.2)'
+    image4.style.transition ='all 0.5s linear'
+ 
+ }
+ red[0].onmouseout = function(){
+   
+      image4.style.transform ='scale(1)'
+      image4.style.transition ='all 0.5s linear'
+ }
+ image4.onmouseover = function(){
+      image4.style.transform ='scale(1.2)'
+      image4.style.transition ='all 0.5s linear'
+ 
+ 
+ }
+ image4.onmouseout = function(){
+     image4.style.transform ='scale(1)'
+     image4.style.transition ='all 0.5s linear'
+ 
+ }
+
+
+ black.onmouseover = function(){
+  
+    image6.style.transform ='scale(1.2)'
+    image6.style.transition ='all 0.5s linear'
+ 
+ }
+ black.onmouseout = function(){
+   
+      image6.style.transform ='scale(1)'
+      image6.style.transition ='all 0.5s linear'
+ }
+ image6.onmouseover = function(){
+      image6.style.transform ='scale(1.2)'
+      image6.style.transition ='all 0.5s linear'
+ 
+ 
+ }
+ image6.onmouseout = function(){
+     image6.style.transform ='scale(1)'
+     image6.style.transition ='all 0.5s linear'
+ 
+ }
+
+ red[1].onmouseover = function(){
+  
+    image10.style.transform ='scale(1.2)'
+    image10.style.transition ='all 0.5s linear'
+ 
+ }
+ red[1].onmouseout = function(){
+   
+      image10.style.transform ='scale(1)'
+      image10.style.transition ='all 0.5s linear'
+ }
+ image10.onmouseover = function(){
+      image10.style.transform ='scale(1.2)'
+      image10.style.transition ='all 0.5s linear'
+ 
+ 
+ }
+ image10.onmouseout = function(){
+     image10.style.transform ='scale(1)'
+     image10.style.transition ='all 0.5s linear'
+ 
+ }
+
+ red[3].onmouseover = function(){
+  
+    image11.style.transform ='scale(1.2)'
+    image11.style.transition ='all 0.5s linear'
+ 
+ }
+ red[3].onmouseout = function(){
+   
+      image11.style.transform ='scale(1)'
+      image11.style.transition ='all 0.5s linear'
+ }
+ image11.onmouseover = function(){
+      image11.style.transform ='scale(1.2)'
+      image11.style.transition ='all 0.5s linear'
+ 
+ 
+ }
+ image11.onmouseout = function(){
+     image11.style.transform ='scale(1)'
+     image11.style.transition ='all 0.5s linear'
+ 
+ }
+
+
+
+ white.onmouseover = function(){
+  
+    image14.style.transform ='scale(1.2)'
+    image14.style.transition ='all 0.5s linear'
+ 
+ }
+ white.onmouseout = function(){
+   
+      image14.style.transform ='scale(1)'
+      image14.style.transition ='all 0.5s linear'
+ }
+ image14.onmouseover = function(){
+      image14.style.transform ='scale(1.2)'
+      image14.style.transition ='all 0.5s linear'
+ 
+ 
+ }
+ image14.onmouseout = function(){
+     image14.style.transform ='scale(1)'
+     image14.style.transition ='all 0.5s linear'
+ 
+ }
+ 
+
+
